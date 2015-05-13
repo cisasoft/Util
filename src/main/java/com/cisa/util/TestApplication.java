dfe38569bd0bd94aa94898663ef10a5c84623d6b
xiaolong.nie <xiaolong.nie@cisasoft.cn>
2015-05-13 14:31:14 +0800

    add validate pic packges

---
+++ src/main/java/com/cisa/util/TestApplication.java
@@ -0,0 +1,10 @@
+package com.cisa.util;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/test/java/com/cisa/test/Test.java
+++ src/test/java/com/cisa/test/Test.java
@@ -1,15 +1,9 @@
 package com.cisa.test;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import com.google.zxing.NotFoundException;
-
 public class Test {
 
-	public static void main(String[] args) throws FileNotFoundException, NotFoundException, IOException {
-
-		
+	public static void main(String[] args){
 
 	}
+	
 }
