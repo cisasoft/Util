0217b2fef91e080f6815cc2662be25609a014c6a
xiaolong.nie <xiaolong.nie@cisasoft.cn>
2015-07-16 13:33:51 +0800

    ver 0.2 milestone

---
+++ src/main/java/com/cisa/util/encrypt/Base64Helper.java
@@ -0,0 +1,38 @@
+package com.cisa.util.encrypt;
+
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ * base64编码工具
+ * @author Xiaolong.Cisa
+ *
+ */
+public class Base64Helper {
+
+	/**
+	 * base64编码
+	 * @param plainText 输入的字符串
+	 * @return 返回编码结果
+	 */
+	public static String encodeStr(String plainText) {
+		byte[] b = plainText.getBytes();
+		Base64 base64 = new Base64();
+		b = base64.encode(b);
+		String s = new String(b);
+		return s;
+	}
+
+	/**
+	 * base64解码
+	 * @param encodeStr 输入密文
+	 * @return 解码的结果
+	 */
+	public static String decodeStr(String encodeStr) {
+		byte[] b = encodeStr.getBytes();
+		Base64 base64 = new Base64();
+		b = base64.decode(b);
+		String s = new String(b);
+		return s;
+	}
+
+}
