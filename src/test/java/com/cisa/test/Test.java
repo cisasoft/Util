a8b52645c1adc35cb34ad266fb2585e17a9655fe
xiaolong.nie <xiaolong.nie@cisasoft.cn>
2015-10-25 14:21:22 +0800

    local pc

--- src/test/java/com/cisa/test/Test.java
+++ src/test/java/com/cisa/test/Test.java
@@ -1,23 +1,19 @@
 package com.cisa.test;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
-import com.cisa.util.file.Html2pdfHelper;
-import com.cisa.util.file.Office2pdfHelper;
-import com.cisa.util.file.ZipHelper;
-import com.cisa.util.image.ImageHelper;
-import com.cisa.util.image.QRCodeHelper;
-import com.cisa.util.mail.MailHelper;
-import com.lowagie.text.Image;
+import com.cisa.util.file.FileHelper;
+import com.cisa.util.time.DateHelper;
 
 public class Test {
 	
 	public static void main(String[] args) throws Exception {
 		
-		Office2pdfHelper.convert2PDF("C:\\test\\4.ppt", "c:/test31/ppt4.pdf");
-		Office2pdfHelper.convert2PDF("C:\\test\\1.pptx", "c:/test31/pptx.pdf");
+		//Office2pdfHelper.convert2PDF("C:\\test\\4.ppt", "c:/test31/ppt4.pdf");
+//		Office2pdfHelper.convert2PDF("C:\\test\\1.pptx", "c:/test1/pptx.pdf");
 		
 //		Office2pdfHelper.convert2PDF("C:\\test\\2.xls", "c:/test31/xls.pdf");
 //		Office2pdfHelper.convert2PDF("C:\\test\\2.xlsx", "c:/test31/xlsx.pdf");
@@ -26,6 +22,28 @@ public class Test {
 //		Office2pdfHelper.convert2PDF("C:\\test\\3.docx", "c:/test31/docx.pdf");
 //		
 //		Office2pdfHelper.convert2PDF("C:\\test\\2.txt", "c:/test31/txt.pdf");
+		
+		
+		List<String> l = FileHelper.getFileFromFolder("c:/test/upload2");
+		
+		Iterator<String> lt = l.iterator();
+		while(lt.hasNext()){
+			String str =  lt.next();
+			//System.out.println(str);
+			File   f   =   new   File(str);
+			String fname = f.getName();//.substring(18);
+			String [] farr = fname.split("\\.");
+			//System.out.println(farr[0]);
+			
+			long   size	=   f.length();	//   大小   bytes 
+			long   modify	=   f.lastModified();	//   修改时间 
+			Date dateOld = new Date(modify);
+			String dt = DateHelper.dateToString(dateOld);
+			//System.out.println(dt);
+			
+			System.out.println("insert into tmp_data values ('"+farr[0]+"','"+dt+"');");
+			
+		}
 		
 	}
 }
